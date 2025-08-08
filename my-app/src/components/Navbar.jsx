df335a068ddb76b03c50cbd665a905995945e321
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2025-08-08 08:56:51 +0500

    Setup
    installation of react vite and tailwindcss,
    and then start designing 1 component.

---
+++ my-app/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return(  <div>
+    {/* Upper Navbar */}
+    <div>
+        <div>
+            <div>
+                <a href="#">Apna Phone</a>
+            </div>
+        </div>
+    </div>
+    {/* Lower Navbar */}
+    <div></div>
+  </div>
+  )
+}
+
+export default Navbar
